c442522a3997a292fc1ee905a30443389e4fdd3f
Abbas ELMAS <iletisim@abbaselmas.com>
2014-05-19 20:34:44 +0300

    Major update

--- ArduinoMegaQuadCopter.ino
+++ ArduinoMegaQuadCopter.ino
@@ -10,44 +10,39 @@
 #include <Servo.h>
 #include "math.h"
 
-/*****PIN Definitions**/
 #define SOL_ON_MOTOR_PINI 12 //3,5,6,9,10,11
 #define SAG_ON_MOTOR_PINI 6
 #define SOL_ARKA_MOTOR_PINI 9
 #define SAG_ARKA_MOTOR_PINI 3
 
-/*****Receiver PINs**********8 channels*/
-#define Aileron_Roll_PIN A15
-#define Elevator_Pitch_PIN A14
-#define Throttle_PIN A13
-#define Rudder_Yaw_PIN A12
-#define AUXCH1 A11
+#define Aileron_Roll_PIN A15 //1
+#define Elevator_Pitch_PIN A14 //2
+#define Throttle_PIN A13 //3
+#define Rudder_Yaw_PIN A12 //4
+/*#define AUXCH1 A11
 #define AUXCH2 A10
 #define AUXCH3 A9
-#define AUXCH4 A8
+#define AUXCH4 A8*/
 
-/**esc ve rc maksimum minimum değerleri**/
 #define MIN 956  //kumanda da T1924956 mod
 #define MAX 1924
 #define SMIN 1000 //servo writemicroseconds minimum
 #define SMAX 2000 //maksimum
-#define ARM_DELAY 3000
-
+#define ARM_DELAY 5000
 #define accZOffset -204 
 
-/*  PID configuration */
 #define PITCH_P_VAL 0.5
 #define PITCH_I_VAL 0
 #define PITCH_D_VAL 1
+
 #define ROLL_P_VAL 2
 #define ROLL_I_VAL 5
 #define ROLL_D_VAL 1
+
 #define YAW_P_VAL 2
 #define YAW_I_VAL 5
 #define YAW_D_VAL 1
 
-
-/* Flight parameters */
 #define PITCH_MIN -30
 #define PITCH_MAX 30
 #define ROLL_MIN -30
@@ -58,109 +53,113 @@
 #define PID_ROLL_INFLUENCE 20
 #define PID_YAW_INFLUENCE 20
 
-#define SENS   s0.0078128 // 8/1024
+#define SENS   0.0078128 // 8/1024
 #define TODEG 57.2957795
 
-/*********10DOF*******/
 ADXL345 acc;  int16_t ax, ay, az;
 BMP085 baro;  float temperature, pressure, altitude;
 HMC5883L mag; int16_t mx, my, mz;
 ITG3200 gyro; int16_t gx, gy, gz;
 
 TinyGPSPlus gps;
-
-/*Servo motorlar* a sol ön, b sağ ön, c sol arka, d sağ arka */
 Servo          a,b,c,d;
 
-/**********variables**************/
 int            velocity;
 float 	       velocityLast;
-
-float          bal_roll, bal_pitch;
-float          bal_axes;
-
-int            va, vb, vc, vd; // motor hızları
-
+float          bal_roll = 0, bal_pitch = 0, bal_axes = 0; // denge durumları +100 -100
+int            va, vb, vc, vd; // motor hızları //1000-2000
 float          ctrl; //rc dğer kontrol
 boolean        interruptLock = false;
-float          ch1, ch1Last, ch2, ch2Last, ch3, ch4, ch4Last, ch5, ch6, ch7, ch8;
-int32_t        lastMicros; //barometre için
-
+float          ch1, ch1Last, ch2, ch2Last, ch3, ch4, ch4Last;//, ch5, ch6, ch7, ch8;
 float          ypr[3]     = {0.0f, 0.0f, 0.0f};
 float          yprLast[3] = {0.0f, 0.0f, 0.0f};
+float          Gx, Gy, Gz, Xh, Yh;
+float 		   t_roll, rollRadian, t_pitch, pitchRadian, cosRoll, sinRoll, cosPitch, sinpitch;
 
-float          Gx, Gy, Gz;
-float		   deltaT = millis();
-
-
-PID yawReg  (&ypr[0], &bal_axes,  &ch4, YAW_P_VAL,   YAW_I_VAL,   YAW_D_VAL,   DIRECT );
-PID pitchReg(&ypr[1], &bal_pitch, &ch2, PITCH_P_VAL, PITCH_I_VAL, PITCH_D_VAL, REVERSE);
-PID rollReg (&ypr[2], &bal_roll,  &ch1, ROLL_P_VAL,  ROLL_I_VAL,  ROLL_D_VAL,  REVERSE);
+PID yawReg  (&ypr[0], &bal_axes,  &ch4,   YAW_P_VAL,   YAW_I_VAL,   YAW_D_VAL,   DIRECT );
+PID pitchReg(&ypr[1], &bal_pitch, &ch2, PITCH_P_VAL, PITCH_I_VAL, PITCH_D_VAL,   REVERSE);
+PID rollReg (&ypr[2], &bal_roll,  &ch1,  ROLL_P_VAL,  ROLL_I_VAL,  ROLL_D_VAL,   REVERSE);
 
 unsigned long  rcLastChange1 = micros();
 unsigned long  rcLastChange2 = micros();
 unsigned long  rcLastChange3 = micros();
 unsigned long  rcLastChange4 = micros(); 
-unsigned long  rcLastChange5 = micros(); 
+/*unsigned long  rcLastChange5 = micros(); 
 unsigned long  rcLastChange6 = micros(); 
 unsigned long  rcLastChange7 = micros(); 
-unsigned long  rcLastChange8 = micros();
-
-float artist;
+unsigned long  rcLastChange8 = micros();*/
 
 void setup(){
   Wire.begin();
+  Serial.begin(57600);
   Serial1.begin(38400);
   initRC();
   initDOF();
   initMotors();
-  initBal();
   initReg();
  }
-
 void loop(){
   
+ /* kitle();
+  va=ch3;
+  vb=ch3;
+  vc=ch3;
+  vd=ch3;
+  birak();
+  updateMotors();
+  */
   getYPR();                   
   computePID();
   calcVel();
   updateMotors();
-
+  Serial.print(ypr[0]);
+  Serial.print("   ");
+  Serial.print(ypr[1]);
+  Serial.print("   ");
+  Serial.print(ypr[2]);
+  Serial.print("   ");
+  Serial.print(va);
+  Serial.print("   ");
+  Serial.print(vb);
+  Serial.print("   ");
+  Serial.print(vc);
+  Serial.print("   ");
+  Serial.println(vd);
+  delay(200);
  }
-
 void getYPR(){
-
-	deltaT = millis() - deltaT;
-    acc.getAcceleration(&ax,&ay,&az);
-    az+=accZOffset; //software calibration for z axis
-
+  
+    acc.getAcceleration(&ax,&ay,&az); az+=accZOffset; //software calibration for z axis
     gyro.getRotation(&gx, &gy, &gz);
     mag.getHeading(&mx, &my, &mz);
 
     Gx = SENS*ax;
     Gy = SENS*ay;
     Gz = SENS*az;
+    
+    ypr[1] = (float)atan(Gy / sqrt((Gx*Gx) + (Gz*Gz)));
+    ypr[2] = (float)atan(Gx / Gz);
 
-    ypr[0] = 0;
-    ypr[1] = atan( Gx / sqrt((Gy*Gy)+(Gz*Gz))) *TODEG;
-    ypr[2] = atan( Gy / sqrt((Gz*Gz)+(Gx*Gx))) *TODEG;
+    Xh= mx*cos(ypr[1]) + mz*sin(ypr[1]);
+    Yh= mx*sin(ypr[2])*sin(ypr[1]) + my*cos(ypr[2]) - mz*sin(ypr[2])*cos(ypr[1]);
+    ypr[0] = (float)atan(Yh/Xh);
+
+    //Convert Radion YPR to Degree
+    ypr[0] = ypr[0] *TODEG;
+    ypr[1] = ypr[1] *TODEG;
+    ypr[2] = ypr[2] *TODEG;
 
  }
-
 void computePID(){
 
   kitle();
-
-  ch1 = floor(ch1/RC_ROUNDING_BASE)*RC_ROUNDING_BASE;
-  ch2 = floor(ch2/RC_ROUNDING_BASE)*RC_ROUNDING_BASE;
-  ch4 = floor(ch4/RC_ROUNDING_BASE)*RC_ROUNDING_BASE;
-
 
   ch2 = map(ch2, MIN, MAX, PITCH_MIN, PITCH_MAX);
   ch1 = map(ch1, MIN, MAX, ROLL_MIN, ROLL_MAX);
   ch4 = map(ch4, MIN, MAX, YAW_MIN, YAW_MAX);
   
   if((ch2 < PITCH_MIN) || (ch2 > PITCH_MAX)) ch2 = ch2Last;
-  if((ch1 < ROLL_MIN)  || (ch1 > ROLL_MAX)) ch1 = ch1Last;
+  if((ch1 < ROLL_MIN)  || (ch1 > ROLL_MAX))  ch1 = ch1Last;
   if((ch4 < YAW_MIN)   || (ch4 > YAW_MAX)) ch4 = ch4Last;
   
   ch1Last = ch1;
@@ -183,33 +182,20 @@ void computePID(){
   birak();
 
  }
-
 void calcVel(){
 
   kitle();
-
-  velocity = map(ch3, MIN, MAX, SMIN, SMAX);
-  
+  	velocity = map(ch3, MIN, MAX, SMIN, SMAX);
   birak();
   
   velocityLast = velocity;
 
-  va = ((((100+bal_pitch)/100)*velocity       +		((100+bal_roll)/100)*velocity)         /2 ) * ((100+bal_axes)/100);
-  vb = ((((100+bal_pitch)/100)*velocity			  +		(abs((-100+bal_roll)/100))*velocity)   /2 ) * (abs(-100+bal_axes)/100);
-  
+  va = ((((100+bal_pitch)/100)*velocity         +		((100+bal_roll)/100)*velocity)         /2 ) * ((100+bal_axes)/100);
+  vb = ((((100+bal_pitch)/100)*velocity			+		(abs((-100+bal_roll)/100))*velocity)   /2 ) * (abs(-100+bal_axes)/100);
   vc = (((abs((-100+bal_pitch)/100))*velocity	+		((100+bal_roll)/100)*velocity)         /2 ) * (abs(-100+bal_axes)/100); 
   vd = (((abs((-100+bal_pitch)/100))*velocity	+		(abs((-100+bal_roll)/100))*velocity)   /2 ) * ((100+bal_axes)/100);
   
  }
-
-void initBal(){
-
-  bal_axes   = 0;
-  bal_roll   = 0;
-  bal_pitch  = 0;
-
- }
-
 void initReg(){
 
   pitchReg.SetMode(AUTOMATIC);
@@ -222,7 +208,6 @@ void initReg(){
   yawReg.SetOutputLimits(-PID_YAW_INFLUENCE, PID_YAW_INFLUENCE);
 
  }
-
 void initDOF(){
   delay(1100);
   acc.initialize();  boolean a = acc.testConnection();
@@ -236,7 +221,6 @@ void initDOF(){
   if(!(a & b & m & g))
     initDOF();
  }
-
 void initMotors(){
   a.attach(SOL_ON_MOTOR_PINI);   b.attach(SAG_ON_MOTOR_PINI);
   c.attach(SOL_ARKA_MOTOR_PINI); d.attach(SAG_ARKA_MOTOR_PINI);
@@ -245,26 +229,22 @@ void initMotors(){
   c.writeMicroseconds(MIN); d.writeMicroseconds(MIN);
   delay(ARM_DELAY);
  }
-
 void updateMotors(){
   a.writeMicroseconds(va);  b.writeMicroseconds(vb);
   c.writeMicroseconds(vc);  d.writeMicroseconds(vd);
  }
-
 void initRC(){
   PCintPort::attachInterrupt(Aileron_Roll_PIN,   rcInterrupt1,   CHANGE);
   PCintPort::attachInterrupt(Elevator_Pitch_PIN, rcInterrupt2,   CHANGE);
   PCintPort::attachInterrupt(Throttle_PIN,       rcInterrupt3,   CHANGE);
   PCintPort::attachInterrupt(Rudder_Yaw_PIN,     rcInterrupt4,   CHANGE); 
-  PCintPort::attachInterrupt(AUXCH1,             rcInterrupt5,   CHANGE); 
+  /*PCintPort::attachInterrupt(AUXCH1,             rcInterrupt5,   CHANGE); 
   PCintPort::attachInterrupt(AUXCH2,             rcInterrupt6,   CHANGE); 
   PCintPort::attachInterrupt(AUXCH3,             rcInterrupt7,   CHANGE); 
-  PCintPort::attachInterrupt(AUXCH4,             rcInterrupt8,   CHANGE);
+  PCintPort::attachInterrupt(AUXCH4,             rcInterrupt8,   CHANGE);*/
  }
-
 void kitle(){ interruptLock = true; } //interrupt locks
 void birak(){ interruptLock = false; }
-
 /*****Interrupt Service Routines ******/
 void rcInterrupt1(){
   if(!interruptLock)
@@ -297,7 +277,7 @@ void rcInterrupt4(){
     if(ctrl<MAX) ch4 = ctrl;
   }
   rcLastChange4 = micros();
- }
+ }/*
 void rcInterrupt5(){
   if(!interruptLock)
   {
@@ -329,5 +309,5 @@ void rcInterrupt8(){
     if(ctrl<MAX) ch8 = ctrl;
   }
   rcLastChange8 = micros();
- }
+ }*/
 /**************************************/
